1747ab8e26a0b86b0fadbb10dc558e38202f324f
yuji1123-hyogo <yuji1123200113@gmail.com>
2025-06-05 00:01:10 +0900

    ユーザー登録機能、ユーザー一覧機能のビューの完成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,12 +6,18 @@ class Admin::UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "ユーザー:#{@user.name}を登録しました"
       redirect_to admin_users_path, status: :see_other
     else
+      flash[:danger] = "ユーザー作成に失敗しました"
       render :new, status: :unprocessable_entity
     end
   end
 
+  def index
+    @users = User.all
+  end
+  
   private
 
   def user_params
