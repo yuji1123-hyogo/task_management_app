c933a02a17de6b5536ceb54afbe2ab579601b50d
yuji1123-hyogo <yuji1123200113@gmail.com>
2025-06-04 23:20:59 +0900

    ユーザーモデル、マイグレーション、登録機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_02_175140) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_04_140007) do
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.boolean "admin", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
